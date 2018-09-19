19ca2672a1529ef1c93635a5cb9cd132fdba45b6
Yann <yannmjl@gmail.com>
2018-09-19 15:13:24 -0600

    hash table problem

---
+++ hash_table.py
@@ -0,0 +1,4 @@
+# given an aary of numbers, find a pair of number that add up to 10
+
+# The function should return or print out the pair of number that add up to 10
+# we will resolve this sulation with a run time of O(n)
