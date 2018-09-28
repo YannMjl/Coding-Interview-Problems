e2e422cac7067441789a6feb9f0bd1848ad7949a
Yann <yannmjl@gmail.com>
2018-09-28 17:40:55 -0600

    added string file problem

---
+++ string.py
@@ -0,0 +1,13 @@
+# Given a string, do some operations
+# a string in python can be traited a list that store characters
+# however, unlike a list, a string is ummutbale. meaning: it can't be changed
+
+# initialiaze a string
+a_string = "This is a strig"
+
+print(a_string)
+
+# given a string, letter = "ABCD"
+# print the second character of the string
+letter = "ABCD"
+print("The second character of the letter string is: ", letter[1])
