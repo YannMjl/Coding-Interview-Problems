78e9e4b604b03da2cf363ef804c501b2cbd890bc
Yann <yannmjl@gmail.com>
2018-09-29 21:33:03 -0600

    solve string reverse examples

--- string.py
+++ string.py
@@ -24,4 +24,26 @@ for i in range(len(letter)):
 # write a fucntion that takes two strings and returns "Ture" 
 # if the two strings are reverse of each other
 
+# given strings
+string_one = "ABCDE"
+Strong_two = "EDCBA"
 
+# to check if these string are reverse of each other
+# we are going to compare the first character of the first string with
+# the last character of the second string and son on until all the charcter 
+# are compared to each other and checked for match.
+
+def IsReverse(given_String_one, given_String_two):
+    for i in range(len(given_String_one)):
+        i_string_two = len(given_String_two) - i - 1
+        if given_String_one[i] != given_String_two[i_string_two]:
+            return False
+    return True
+
+# let try our function
+print(IsReverse(string_one, Strong_two)) # should return True
+print(IsReverse("ADC", "CDA")) # should return True
+print(IsReverse("DGCF", "CFGD")) # should return False
+
+
+ 
