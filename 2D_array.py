107bba9a9fc118da17bb3e49b7aa25e9cb38f2a6
Yann <yannmjl@gmail.com>
2018-10-01 19:59:55 -0600

    fix error on hash table and 2D array

--- 2D_array.py
+++ 2D_array.py
@@ -2,7 +2,8 @@
 # Is implemented as array of array or list of list in python
 
 # initialize an 2D array
-array_2D = [[1, 2, 3, 4], ["a", "b", "c", "d"]]
+array_2D = [[1, 2, 3, 4],
+            ["a", "b", "c", "d"]]
 
 # or 
 array_2d = [[5,6,7,8],
